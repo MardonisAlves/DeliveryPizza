445a8f250a7a8693b4ab66e2a0551b7b67bccff6
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2019-06-17 16:46:30 -0300

    Conexao com heroku

--- app/settings.php
+++ app/settings.php
@@ -23,10 +23,10 @@
                     ],
                     'connection' => [
                         'driver'   => 'pdo_mysql',
-                        'host'     => 'localhost',
-                        'dbname'   => 'test',
-                        'user'     => 'root',
-                        'password' => '',
+                        'host'     => 'ec2-54-163-230-199.compute-1.amazonaws.com',
+                        'dbname'   => 'df5gjbqd0bajje',
+                        'user'     => 'zmtywefdmlubee',
+                        'password' => '3fca1e8feebd65d3497207ccfde1c490b5d53edb6dd1bb38552a21e8ef02e974',
                     ]
                 ]
 
