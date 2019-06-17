6d1e5b102cb39a828647048ec3608fc136fd5272
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2019-06-17 18:17:26 -0300

    Teste add

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -43,10 +43,10 @@ public function login($request, $response, $args)
 
   $user = new User();
   $this->em->persist($user);
-  $user->setFullName("Mardonis Alves");
-  $user->setEmail("mardonisgp@gmail.com");
-  $user->setTypeUser("Admin");
-  $user->setSenha(password_hash('qwe123qwe@',PASSWORD_DEFAULT));
+  $user->setFullName = "Mardonis Alves" ;
+  $user->setEmail ="mardonisgp@gmail.com";
+  $user->setTypeUser = "Admin";
+  $user->setSenha = (password_hash('qwe123qwe@',PASSWORD_DEFAULT));
   $this->em->flush();
 
 try{
