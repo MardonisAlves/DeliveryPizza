c1dfc043448d8da827e583963d6037b937cc3a2d
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2019-07-21 20:27:40 -0300

    associação_entities

--- app/Model/UsersClientes.php
+++ app/Model/UsersClientes.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\Mapping\Id;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping\JoinColumn;
 /**
 * Blog UsersClientes entity
 *
