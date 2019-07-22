a4de1cdc613dab9e53df7729f31d753a093da747
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2019-07-22 01:40:12 -0300

    GET_UPDATE_TESTE

--- app/Validate/Validate.php
+++ app/Validate/Validate.php
@@ -28,7 +28,7 @@ public function validate(Request $request , Response $response , $flash)
 
   //if(isset($_SESSION['typeUser']) == 'admin'){
 
-  $contact =  $this->em->getRepository('App\Model\Users')->findAll();
+  $contact =  $this->em->getRepository('App\Model\UsersClientes')->findAll();
   $json = json_encode($contact);
   var_dump($json);
   /*
