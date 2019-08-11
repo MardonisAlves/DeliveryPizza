0889f2d996d4b9ba5d6f37590abf77c6d8df044d
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2019-08-11 17:03:52 -0300

    validatehome

--- app/Validate/Validate.php
+++ app/Validate/Validate.php
@@ -69,8 +69,10 @@ if($contact){
          $_SESSION["email"] = $l->getEmail();
 
        //return $this->container->view->render($response ,'admin/home.twig',Array('contact' => $contact));
-        $url = $this->container->get('router')->pathFor('home');
-         return $response->withStatus(302)->withHeader('Location', $url);
+        //$url = $this->container->get('router')->pathFor('home');
+        //return $response->withStatus(302)->withHeader('Location', $url);
+
+         return $response->withRedirect('/home', 200);
 
       }else{
 
