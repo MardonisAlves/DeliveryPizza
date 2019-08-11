378423781d84730ca054944c3191ab2c96353db6
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2019-08-10 22:26:28 -0300

    valiDupmicateEmail

--- app/Validate/Validate.php
+++ app/Validate/Validate.php
@@ -219,7 +219,7 @@ public function validatedelete($request  , $response , $args)
 
 public function validatenewuser($request, $response, $args)
 {
-  if(("admin" == $_SESSION["typeUser"]))  
+  if(isset($_SESSION["typeUser"]) == "cliente")  
   {
     return $this->container->view->render($response ,'admin/newuser.twig');
         
