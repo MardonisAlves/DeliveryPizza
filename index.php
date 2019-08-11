5f2ccb9c1714c50f485ab45391d1a75375e2c3b4
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2019-08-11 14:25:48 -0300

    validatehome

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
 
 
-session_cache_limiter('private');
-//$cache_limiter = session_cache_limiter();
-
-/* define o prazo do cache em 30 minutos */
-session_cache_expire(30);
-//$cache_expire = session_cache_expire();
-
 session_start();
 
 
