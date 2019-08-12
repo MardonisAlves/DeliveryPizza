39ca4414680478214ada1c3a65e56dd6ec6af609
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2019-08-12 16:12:41 -0300

    valiadtedeleteuser

--- app/Controller/BebidaController.php
+++ app/Controller/BebidaController.php
@@ -93,7 +93,6 @@ public function insert_bebidas(Request  $request, Response $response,  array $ar
     $this->em->persist($Produtos);
     $Produtos->setName($_POST['name']);
     $Produtos->setDateValidade(new \DateTime($_POST['date']));
-    $Produtos->setUrlImage($_FILES['url_image']['name']);
     $Produtos->setPorcentagemVenda($_POST['porcentagemVenda']);
     $Produtos->setPrecoCompra($_POST['preco_compra']);
     $Produtos->setDescricao($_POST['descricao']);
