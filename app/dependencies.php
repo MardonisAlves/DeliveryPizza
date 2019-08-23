dc1c11757428bef77120b9a43ab0c8ad690f9e0d
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2019-08-23 20:48:26 -0300

    add-sessionslim-twig

--- app/dependencies.php
+++ app/dependencies.php
@@ -7,11 +7,14 @@ $container = $app->getContainer();
 $container['view'] = function ($container) {
 
     $view = new \Slim\Views\Twig(__DIR__.'/resources/views', ['cache' => false,]);
+     $view->getEnvironment()->addGlobal('session', $_SESSION);
     $view->addExtension(new \Slim\Views\TwigExtension(
 
         $container->router,
         $container->request->getUri()
 ));
+    
+
     return $view;
 
 };
