239dbfa9a38e503cc4cbcf6eedd0253f572453a4
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2019-09-08 07:48:26 -0300

    deploy-heroku-condfi

--- app/settings.php
+++ app/settings.php
@@ -28,7 +28,7 @@
                         'host'     => 'ec2-50-19-254-63.compute-1.amazonaws.com',
                         'dbname'   => 'db8s9vh8d24522',
                         'user'     => 'ouqgizhbemfzbl',
-                        'password' => 'da2135d9e9e80620c9onn2bc66136e9677b3b2b08be0a7b18f264c80ea425352f2ec',
+                        'password' => 'da2135d9e9e80620c92bc66136e9677b3b2b08be0a7b18f264c80ea425352f2e',
                     ]
                 ]
 
