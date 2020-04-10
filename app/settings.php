6544c9ba379cf439fbce75d41a3c85f0daede85b
Mardonis Alves <mardonisgp@gmail.com>
2020-04-10 01:08:56 -0300

    setting-config-pdo

--- app/settings.php
+++ app/settings.php
@@ -28,10 +28,10 @@
                     ],
                     'connection' => [
                         'driver'   => 'pdo_pgsql',
-                        'host'     => 'ec2-50-19-254-63.compute-1.amazonaws.com',
-                        'dbname'   => 'db8s9vh8d24522',
-                        'user'     => 'ouqgizhbemfzbl',
-                        'password' => 'da2135d9e9e80620c92bc66136e9677b3b2b08be0a7b18f264c80ea425352f2e',
+                        'host'     => 'ec2-3-91-112-166.compute-1.amazonaws.com',
+                        'dbname'   => 'dbi2apjua8v7h8',
+                        'user'     => 'qkufycndgpityd',
+                        'password' => '7ac616e99c17641f38c2acab3426c82ae191f8e2597ddcbd6e7b0fcf10edadf4',
                     ]
                     ],
                     "db" => [
