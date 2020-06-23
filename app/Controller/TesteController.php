5d48e44b95b8714aca03e6f1659b6365ebb27232
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2020-06-22 21:51:23 -0300

    test-json

--- app/Controller/TesteController.php
+++ app/Controller/TesteController.php
@@ -59,10 +59,10 @@ $manager = $this->em->getRepository('\App\Model\Users');
 $users = $manager->findAll();
 
         foreach ($users as $user) {
-        $data  = array(
+        $data =  $users = array(
                        'email' => $user->getEmail(),
                        'Id' => $user->getId(),
-                        'nome' => $user-> getNome()
+                         'nome' => $user-> getNome()
                       );
         return $response->withJson($data , 200);   // response json com withJsons
 }
