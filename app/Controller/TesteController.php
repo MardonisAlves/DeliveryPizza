9e908bb1781afba0e8f1ceafebdd179aa44c659d
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2020-06-22 22:34:36 -0300

    test-json

--- app/Controller/TesteController.php
+++ app/Controller/TesteController.php
@@ -58,7 +58,7 @@ header('Access-Control-Allow-Origin: *'); // Este cabeçalho aceita qualquer req
 $manager = $this->em->getRepository('\App\Model\Users')->findAll();
 
 
-        foreach ($manager as $user) {
+        foreach ($manager as $user->fetch()) {
       /*  $data = array(
                       'Id' => $user->getId(),
                        'nome' => $user->getNome(),
