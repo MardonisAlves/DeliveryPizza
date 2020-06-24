e5d4ecdfb60ac9c686585ed0d379c0bd6ccd77d5
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2020-06-23 22:17:27 -0300

    header

--- app/Controller/TesteController.php
+++ app/Controller/TesteController.php
@@ -80,7 +80,7 @@ public function user(Request $request , Response $response , $args)
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json; charset=utf-8');
     header("Access-Control-Allow-Headers:","Content-Type");
-    header("Access-Control-Allow-Methods:","GET");
+    header("Access-Control-Allow-Methods:","POST");
     $json = file_get_contents('php://input');
     $obj = json_decode($json);
 
