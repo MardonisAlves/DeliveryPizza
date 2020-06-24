0647befbe990aa2cccb8ccd3fd31ca1587f8a945
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2020-06-23 21:23:11 -0300

    header

--- app/Controller/TesteController.php
+++ app/Controller/TesteController.php
@@ -75,7 +75,7 @@ $manager = $this->em->getRepository('\App\Model\Users')->findAll();
 // new User
 public function user(Request $request , Response $response , $args)
 {
-    //header('Access-Control-Allow-Origin: *'); // Este cabeçalho aceita qualquer requisição
+    header('Access-Control-Allow-Origin: *'); // Este cabeçalho aceita qualquer requisição
     //get json
     //header('Content-Type: application/json; charset=utf-8');
     //header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
