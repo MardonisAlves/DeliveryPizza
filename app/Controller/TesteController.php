40c3a77f4938c4c0bd1b883bc9fda74836c14229
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2020-06-25 21:51:14 -0300

    delete-id

--- app/Controller/TesteController.php
+++ app/Controller/TesteController.php
@@ -106,7 +106,7 @@ public function updateuser(Request $request , Response $response , $args)
 // delete user
 public function deleteuser(Request $request , Response $response , $args)
 {
-  header("Access-Control-Allow-Origin: http://localhost:8080");
+  header("Access-Control-Allow-Origin: https://infinite-springs-64835.herokuapp.com/delete");
   $data = $array = array('id' => $args );
   return $response->withJson($data , 200);
 }
