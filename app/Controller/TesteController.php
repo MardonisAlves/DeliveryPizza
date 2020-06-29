f5c3878e4142e39d8b62eba6f79b67045f29aa5b
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2020-06-29 19:45:40 -0300

    findOneBy

--- app/Controller/TesteController.php
+++ app/Controller/TesteController.php
@@ -112,7 +112,7 @@ public function deleteuser(Request $request , Response $response , $args)
     header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Origin, Authorization');
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS');
 
-  $manager = $this->em->findOneBy('App\Model\Users' , $args['id']);
+  $manager = $this->em->getRepository('App\Model\Users')->findOneBy($args['id']);
   $this->em->remove($manager);
   $this->em->flush();
 
