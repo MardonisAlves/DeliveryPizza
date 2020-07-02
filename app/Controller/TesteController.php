37d4fe53d39aea58b0075d08c252f8f7105be979
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2020-07-02 01:13:41 -0300

    response-application/javascript

--- app/Controller/TesteController.php
+++ app/Controller/TesteController.php
@@ -68,11 +68,7 @@ $manager = $this->em->getRepository('\App\Model\Users')->findAll();
 
     //  return $response->withJson($alldata, 200);
 
-  return  $response->header('Access-Control-Allow-Origin: *')
-    header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Origin, Authorization')
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS')
-    withHeader('Content-type', 'application/json')
-    withJson($alldata, 200);
+  
 }
 
 
