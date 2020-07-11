90512d800ca2a659fc7c37aa6fa28a5068c14957
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2020-07-11 12:46:59 -0300

    reset-password

--- app/Controller/SenhaController.php
+++ app/Controller/SenhaController.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\Controller; 
+namespace App\Controller;
 
 use App\Model\Users;
 use App\Validate\Validate;
@@ -10,22 +10,22 @@ use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
 
 
-class SenhaController 
+class SenhaController
 {
       private $db;
       private $container;
       private $flash;
       private $session;
-      
+
 public function __construct($container , $db ,$flash , $session )
 {
           $this->db = $db;
           $this->container=$container;
           $this->flash = $flash;
           $this->session=$session;
-         
 
-           
+
+
 }
 
 // Recuperar senha
@@ -36,33 +36,28 @@ public  function  recu_form(Request $request, Response $response, $args)
         'admin/login/recu_form.twig');
 }
 
-public function enviartoken(Request $request, Response $response, $args) 
-{   
+public function enviartoken(Request $request, Response $response, $args)
+{
     // selecionar o user by email
-  $Users = new Users();
-  $Users->setContainer($this->container);
-  $Users->setSession($this->session);
-  $Users->setConnection($this->db);
-$email =   $Users->getuserByemail($_POST['email']);
+  $users = $this->db->getRepository('\App\Model\Users')->findOneBy(array('email') => $-POST['email']);
 
-    
-    if($email)
+    if($users)
     {
-        foreach($email  as $sms)
+        foreach($users  as $sms)
         {
-            
-        
+
+
         $stringhas = "$#@.;0dq>=+/8*&&";
         $tk = password_hash($stringhas,PASSWORD_DEFAULT);
 
 
 
-        // criar session 
+        // criar session
       $this->session->set('tk', $tk);
       $this->session->set('id', $sms['id']);
-        
-$message = "<a href='http://localhost:8080/atu_senha?tk=$_SESSION[tk]'>Click Aqui</a>";
-        
+
+$message = "<a href='https://infinite-springs-64835.herokuapp.com/atu_senha?tk=$_SESSION[tk]'>Click Aqui</a>";
+
         $mail = new PHPMailer();
         $mail->IsSMTP(); // envia por SMTP
         $mail->CharSet = 'UTF-8';
@@ -74,52 +69,52 @@ $message = "<a href='http://localhost:8080/atu_senha?tk=$_SESSION[tk]'>Click Aqu
         $mail->SMTPAuth = true; // Caso o servidor SMTP precise de autenticação
         $mail->Username = "mardonisgp@gmail.com"; // SMTP username
         $mail->Password = "#qwe123qwe@"; // SMTP password
-        
+
         $mail->From = "mardonisgp@gmail.com"; // From
         $mail->FromName = "Mardonis Alves B" ; // Nome de quem envia o email
-        
+
         $mail->AddAddress($_POST['email'], $sms['nome']); // Email e nome de quem receberá //Responder
         $mail->WordWrap = 50; // Definir quebra de linha
-        
-       
-        
+
+
+
         $mail->IsHTML = true ; // Enviar como HTML
         $mail->Subject = "Senha" ; // Assunto
         $mail->Body =  $message ; //Corpo da mensagem caso seja HTML
         $mail->AltBody = "ola" ; //PlainText, para caso quem receber o email não aceite o corpo HTML
-                   
-            
+
+
         if(!$mail->Send()) // Envia o email
         {
             echo "Erro no envio da mensagem";
         }
-        
-        
+
+
         $this->flash->addMessageNow('msg', 'Verifique o seu email para continuar');
         $messages = $this->flash->getMessages();
         return $this->container->view->render($response ,'admin/login/recu_form.twig',Array('messages' => $messages));
-        
+
         }
     }else{
-        
+
         $this->flash->addMessageNow('msg', 'E-mail não encontrado');
         $messages = $this->flash->getMessages();
-        
+
         return $this->container->view->render(
             $response ,
             'admin/login/recu_form.twig',
             Array(
                 'messages' => $messages));
          }
-    
+
     // criar um token com o email
     // enviar o token por email com o link com method get id=token
-    
-         
-    
-         
-         
-         
+
+
+
+
+
+
 
 }
 // atu_senha
@@ -136,21 +131,21 @@ return $this->container->view->render($response ,'admin/login/atu_senha.twig', A
       $this->flash->addMessageNow('msg', 'Acesso negado');
      $messages = $this->flash->getMessages();
      return $this->container->view->render($response ,'admin/login/loginCliente.twig', Array('messages' => $messages));
-   
+
  }
 }
 
 
- 
+
 
 
 public function updatesenha(Request $request, Response $response, $args)
 {
-  
+
     // verificar os canpos vazios
     if($_POST['senha'] != $_POST['repetsenha'])
     {
-        
+
         $this->flash->addMessageNow('msg', 'As senha não conferem');
         $messages = $this->flash->getMessages();
         return $this->container->view->render(
@@ -158,7 +153,7 @@ public function updatesenha(Request $request, Response $response, $args)
             'admin/login/atu_senha.twig',
             Array( 'messages' => $messages));
     }
-    
+
     // verifica se existe o token
     if(isset($_SESSION['tk']))
     {
@@ -169,19 +164,19 @@ public function updatesenha(Request $request, Response $response, $args)
         $email =  password_hash($_POST['email'], PASSWORD_DEFAULT);
         $Users->updatsenha($email);
 
-        
+
         //$users->setSenha(password_hash($_POST["senha"] , PASSWORD_DEFAULT));
-       
+
         // apagar a cookie tk
        unset($_SESSION['tk']);
        unset($_SESSION['id']);
-       
+
        $this->flash->addMessageNow('msg', 'Sua senha Foi atualizada com sucesso!');
        $messages = $this->flash->getMessages();
        return $this->container->view->render($response ,'admin/login/loginCliente.twig', Array('messages' => $messages));
-       
+
     }else {
-        
+
         $this->flash->addMessageNow('msg', 'Seu token esta invalido!');
         $messages = $this->flash->getMessages();
         return $this->container->view->render($response ,'admin/login/loginCliente.twig', Array('messages' => $messages));
@@ -189,5 +184,3 @@ public function updatesenha(Request $request, Response $response, $args)
 }
 
 }
-
-   
