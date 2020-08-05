1e837bd2e5bdf5a153c82bca0a5811f6a1103a22
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2020-08-04 21:03:09 -0300

    listallpizza

--- app/Controller/api/PizzaControllerApi.php
+++ app/Controller/api/PizzaControllerApi.php
@@ -59,7 +59,7 @@ public function listcardapioid(Request  $request, Response $response, $args)
     header('Access-Control-Allow-Origin: *'); 
     $categoria = $this->em->find('App\Model\Categorias' , $args['id']);
 
-   $pizza = $this->em->getRepository('App\Model\Pizza')->findBy(array('categorias' => $categoria->getId() ));
+   $pizza = $this->em->getRepository('App\Model\Pizza')->findBy(array('categorias' => $$args['id'] ));
   
     $data = Array();
     foreach ($pizza as $card) {
